8d35a07ccefbecf075538a4efaa1fd55cc4675d5
marcog <marcog@192.168.20.188>
2017-10-12 11:12:24 +0200

    ver. 1.4

---
+++ BJ2/src/Card.java
@@ -0,0 +1,50 @@
+
+
+public class Card {
+
+    private Suit suit;
+    private Rank rank;
+
+
+    public Card(Rank rank, Suit suit) {
+        super();
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public String toString() {
+
+        String e;
+
+
+        if (rank.getRankString().equals("10"))
+        e =  "" +
+                "┏───────┓\n" +
+                "┃       ┃\n" +
+                "┃       ┃\n" +
+                "┃  " + rank.getRankString() + suit.getSuit() + "  ┃\n" +
+                "┃       ┃\n" +
+                "┃       ┃\n" +
+                "┗───────┛";
+        else
+            e= "" +
+                    "┏───────┓\n" +
+                    "┃       ┃\n" +
+                    "┃       ┃\n" +
+                    "┃  " + rank.getRankString() + " " + suit.getSuit() + "  ┃\n" +
+                    "┃       ┃\n" +
+                    "┃       ┃\n" +
+                    "┗───────┛";
+
+        return e;
+
+    }
+}
