7949595ffc1758b7658f2edb5019e199f713e65c
clarissa323 <clarissa323@gmail.com>
2017-10-12 13:28:13 +0200

    Test9

--- bj3/src/GameRunner.java
+++ bj3/src/GameRunner.java
@@ -17,7 +17,7 @@ public class GameRunner {
 /////// Preface for new variables, Objects, etc.//////////////////////
         System.out.println("───────────────────────────────────────");
         System.out.println("Welcome to BLACKJACK created by Team 2!");
-        System.out.println("──────────────────────────────────────────────────────────────────────────────");
+        System.out.println("───────────────────────────────────────────");
         Game game = new Game();
         Player play = new Player();
         Player[] players = play.multiPlayers();   /// multi players;
